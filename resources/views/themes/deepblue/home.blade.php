a0f1e0076d072c9ef500341518417d21debd9d01
Rajiur Rahman <rajiurrahmanraj2021@gmail.com>
2022-09-17 17:52:58 +0600

    initital commit

---
+++ resources/views/themes/deepblue/home.blade.php
@@ -0,0 +1,18 @@
+@extends($theme.'layouts.app')
+@section('title',trans('Home'))
+
+@section('content')
+    @include($theme.'partials.heroBanner')
+
+    @include($theme.'sections.feature')
+    @include($theme.'sections.about-us')
+
+    @include($theme.'sections.why-chose-us')
+    @include($theme.'sections.how-it-work')
+    @include($theme.'sections.testimonial')
+    @include($theme.'sections.news-letter')
+    @include($theme.'sections.blog')
+
+    @include($theme.'sections.faq')
+    @include($theme.'sections.we-accept')
+@endsection
